68c93b8f187920aae2e79dd9ec52423dcd049c4d
jeffigy <ejeffydev@gmail.com>
2023-11-29 09:48:27 +0800

    changed customSection minH

--- src/components/Sections/CustomSection.tsx
+++ src/components/Sections/CustomSection.tsx
@@ -17,7 +17,8 @@ const CustomSection: React.FC<CustomSectionProps> = ({
       <Flex
         direction={"column"}
         align={"center"}
-        minH={"calc(100vh - 56px)"}
+        // minH={"calc(100vh - 56px)"}
+        minH={"500px"}
         // py={"100px"}
         mb={{ base: "50px", md: "100px" }}
       >
